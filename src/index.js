50f12648d02ad3ca56a576dac0303d4104e211c8
harshitanst <harshita.2024@nst.rishihood.edu.in>
2025-08-21 12:50:01 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const inputFilePath= path.join(__dirname,'input.txt');
+const outputFilePath = path.join(__dirname, "output.txt");
+fs.readFile(inputFilePath,'utf-8',(err,data)=>{
+    if(err){
+        console.log('error aagya')
+        return;
+    }
+    const lines = data.trim().split("\n");
+    let total = 0;
+    lines.forEach(line=>{
+        const parts = line.split(" ");
+        const number= Number(parts[1])
+        total+=number
+    });
+    fs.writeFile(outputFilePath,total.toString(),'utf-8',(err)=>{
+        if(err){
+            console.log('eeror aaya')
+            return ;
+        }
+        console.log('ouput aagyaa');
+    })
+})
