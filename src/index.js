5798929369836f7cd5b107f8a37e993970e612d3
harshitanst <harshita.2024@nst.rishihood.edu.in>
2025-08-25 16:10:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const inputFilePath= path.join(__dirname,'input.txt');
+
+const inputFilePath = path.join(__dirname, 'input.txt');
 const outputFilePath = path.join(__dirname, "output.txt");
-fs.readFileSync(inputFilePath,'utf-8',(err,data)=>{
-    if(err){
-        console.log('error aagya')
-        return;
-    }
+
+try {
+    const data = fs.readFileSync(inputFilePath, 'utf-8');
     const lines = data.trim().split("\n");
     let total = 0;
-    lines.forEach(line=>{
+    lines.forEach(line => {
         const parts = line.split(" ");
-        const number= Number(parts[1])
-        total+=number
+        const number = Number(parts[1]);
+        total += number;
     });
-    fs.writeFileSync(outputFilePath,total.toString(),'utf-8',(err)=>{
-        if(err){
-            console.log('eeror aaya')
-            return ;
-        }
-        console.log('ouput aagyaa');
-    })
-})
+
+    fs.writeFileSync(outputFilePath, total.toString(), 'utf-8');
+    console.log('output aagya');
+} catch (err) {
+    console.log('error aagya');
+}
+
 
